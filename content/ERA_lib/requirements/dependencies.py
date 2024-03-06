07c2f051d22366f1b8c6c75ff618dd01033d36e7
jakejones <jakeljones95@gmail.com>
2024-03-06 12:48:11 +0800

    Fix package versions

--- content/ERA_lib/requirements/dependencies.py
+++ content/ERA_lib/requirements/dependencies.py
@@ -3,12 +3,13 @@ import piplite
 async def install():
 
     await piplite.install([
-        "widgetsnbextension==4.0.10",
-        "nbformat==5.9.2",
-        "ipywidgets==8.1.2",
-        "numpy==1.26.4",
-        "matplotlib==3.8.3",
-        "pandas==2.2.1",
-        "plotly==5.19.0",
-        "astropy==6.0.0"
+        "widgetsnbextension==4.0.10", # included (see: jupyter-lite.json)
+        "nbformat==5.9.2",            # included (see: jupyter-lite.json)
+        "ipywidgets==8.1.2",          # included (see: jupyter-lite.json)
+        "plotly==5.19.0",             # included (see: jupyter-lite.json)
+        
+        "numpy==1.26.1",              # download at runtime (platform/os specific)
+        "matplotlib==3.5.2",          # download at runtime (platform/os specific)
+        "pandas==1.5.3",              # download at runtime (platform/os specific)
+        "astropy==5.3.2"              # download at runtime (platform/os specific)
     ])
